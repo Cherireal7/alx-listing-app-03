95f483b1b3212111f26660db999e2992409728ba
Cherinet <cheridemeke7@gmail.com>
2025-07-17 13:11:23 +0300

    Initial commit for alx-listing-app-03

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-100 text-center py-6 mt-12">
+      <p className="text-gray-600">&copy; 2025 YourCompany. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
